2d4fc04973c97ea568bd5407e2c6cc58c3d100db
casper <casper@DESKTOP-2PU1K7L>
2022-08-02 01:42:22 +0300

    Add project files.

---
+++ OrganikUrunlerWeb/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OrganikUrunlerWeb.Models.entity;
+
+namespace OrganikUrunlerWeb.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        organikbesinlerEntities db = new organikbesinlerEntities();
+        public ActionResult Index()
+        {
+            
+            var urunler = db.tbl_urunler.ToList();
+            return View(urunler);
+        }
+    }
+}
